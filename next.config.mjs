b05a61f9c781a6282607a89662362e984df243fc
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2025-11-20 18:15:56 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+    unoptimized: process.env.NODE_ENV === 'development',
+  },
+}
 
-export default nextConfig;
+export default nextConfig
